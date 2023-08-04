fdc1c4dac70c559563aa596939347d63b7802b7a
Melnikova26 <mmelnekowa@gmail.com>
2023-08-04 17:52:25 +0500

    fix: fixed docker

--- server/index.ts
+++ server/index.ts
@@ -2,24 +2,17 @@ import { Express, Request, Response } from "express";
 import express from "express";
 import dotenv from "dotenv";
 import storiesRouter from "./routes/routes";
-import cors from "cors";
 
 dotenv.config();
 
 export const app: Express = express();
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+const PORT = 8000;
 
 app.get("/ping", (req: Request, res: Response) => {
   res.send("Pong");
 });
 
-app.use("/", storiesRouter);
+app.use("/api", storiesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
